955330ddc2fdeab50d10223293cf0ee194bf6bb3
DanielPincu <ghepin01@easv365.dk>
2026-02-22 13:03:40 +0100

    Joi  validation, updated

--- src/models/qso.model.ts
+++ src/models/qso.model.ts
@@ -5,10 +5,15 @@ const QSOSchema = new Schema(
     callsign: { type: String, required: true },
     band: { type: String, required: true },
     mode: { type: String, required: true },
+
     rstSent: { type: String },
     rstReceived: { type: String },
+
     qsoDate: { type: Date, required: true },
     operatorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+
+    qth: { type: String },
+    locator: { type: String },
   },
   { timestamps: true }
 )
